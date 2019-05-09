5884d0ac2bb8225bda29dd4f8bc1442e971ef6d9
Scott Gale <galejscott@gmail.com>
2019-05-09 11:27:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,7 @@ def age_appropriate_birthday(birthday_kids)
     if age < 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     else
-      "You are too olf for this."
+      "You are too old for this."
     end
   end
 end
