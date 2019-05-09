d2f1aa6429813ac31eb42deb2059040e9adaa8e3
Scott Gale <galejscott@gmail.com>
2019-05-09 11:26:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,14 +3,14 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-=begin
+
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |name, age|
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
-=end
+
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age < 12
